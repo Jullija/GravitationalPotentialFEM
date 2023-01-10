cb7f8eaf6919e60da8f685d3727ef4aacc5f17c7
Julija <juleczek2002@gmail.com>
2023-01-10 13:44:03 +0100

    Gravitational Potential

--- gravitationalPotential.m
+++ gravitationalPotential.m
@@ -3,11 +3,11 @@ a = 0;
 b = 3;
 
 h = (b-a)/n;
-funP1 = @(h) 1./(h.*h);
-funP2 = @(h) -1./(h.*h);
-P1 = integral( funP1, 0, 2*h);
-G = 6.673e-11;
-const = 4 * pi * G;
+funP1 =  1./(h.*h);
+funP2 = -1./(h.*h);
+P1 = funP1 * 2*h;
+P2 = funP2 * h;
+
 
 %główna macierz
 M=sparse(n-1,n-1);
@@ -15,45 +15,24 @@ M=sparse(n-1,n-1);
 %macierz L(ej)
 RHS = zeros(n-1, 1);
 
-%uzupełnianie macierzy
-for r = 1 : n-1 
-    for c = 1 : n-1  
-        
-        if r == c
-            M(r, c) = P1;
-        end
 
-        if r == c + 1
-            M(r, c) = integral(funP2, c*h, r*h);
-        end
+%uzupełnianie głównej macierzy
+M(1, 1) = P1;
+M(1, 2) = P2;
 
-        if r + 1 == c 
-            M(r, c) = integral(funP2, r*h, c*h);
-        end
-    end
+for k = 2 : n-2
+   M( k , k-1) = P2;
+   M ( k , k ) = P1 ;
+   M ( k , k+1) = P2;
 end
 
+M (n-1,n-2) = P2;
+M (n-1,n-1) = P1 ;
+
+
 %macierz L(ej)
-for i = 2 : n 
-    sum = 0;
-
-    %część z całką pierwszą
-    sum = sum + 1/3 * integral(@(h) 1./h, h*(i-1), h*i);
-    sum = sum + 1/3 * integral(@(h) 1./h, h*i, h*(i+1));
-
-    %część z całką drugą 
-    %(tutaj całkujemy na przedziale 1-2, stąd min oraz max)
-    %korzystamy ze wzoru na ei
-    temp = i;
-    if max(h*(i - 1), 1) < min(h*i, 2)
-        sum = sum + const * integral(@(h) 1./h.*h - temp + 1, max(h*(i - 1), 1), min(h*i, 2));
-    end
-
-     if max(h*i, 1) < min(h*(i + 1), 2) 
-        sum = sum + const * integral(@(h) temp - 1./h.*h  + 1, max(h*(i - 1), 1), min(h*i, 2));
-    end
-
-    RHS(i-1 , 1) = sum;
+for k = 1 : n-1 
+    RHS ( k , 1 ) = f(a+(k-1)*h)*h/6 + f(a+k*h)*2*h/3 + f(a+(k+1)*h)*h/6;
 end
 
 result = M\RHS;
@@ -64,11 +43,27 @@ for k = 2 : n
    values(k)=result(k-1);
 end
 
+for k = 1: n+1
+    values(k) = values(k) + shift(a + (k-1)*h);
+end
+
 x=points ;
 y=values ;
 
 plot(x, y);
 
+end
 
+function y=f(x)
+    G = 6.673e-11;
+    const = 4 * pi * G;
+   if (x > 1 && x <=2)
+       y = const * 1;
+   else
+       y = 0;
+   end
+end
 
+function y=shift(x)
+    y=5 - x/3;
 end
